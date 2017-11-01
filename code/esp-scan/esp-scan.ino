0f0fd0da3cfc18d61ab674814b019979629919df
chris24gutsch <33132055+chris24gutsch@users.noreply.github.com>
2017-11-01 18:51:04 -0500

    Add files via upload

--- code/esp-scan/esp-scan.ino
+++ code/esp-scan/esp-scan.ino
@@ -10,11 +10,10 @@ void setup()
 
   WiFi.mode(WIFI_STA);
   WiFi.disconnect();
-  delay(100);
+  delay(2000); // Leaves the user 2 seconds to get the COM window open to view the MAC address
 
   
   WiFi.macAddress(mac);
-  /*
   Serial.println("");
   Serial.print("MAC: ");
   Serial.print(mac[0],HEX);
@@ -28,27 +27,13 @@ void setup()
   Serial.print(mac[4],HEX);
   Serial.print(":");
   Serial.println(mac[5],HEX);
-  */
+
 }
 
 void loop()
 {  
   if (count == 0) {
-    Serial.println("");
-    Serial.print("MAC: ");
-    Serial.print(mac[0],HEX);
-    Serial.print(":");
-    Serial.print(mac[1],HEX);
-    Serial.print(":");
-    Serial.print(mac[2],HEX);
-    Serial.print(":");
-    Serial.print(mac[3],HEX);
-    Serial.print(":");
-    Serial.print(mac[4],HEX);
-    Serial.print(":");
-    Serial.println(mac[5],HEX); 
-    
-    Serial.print("Scan start ... ");
+    Serial.println("Scan start ... ");
     
     int n = WiFi.scanNetworks();
     Serial.print(n);
